68c021b66fb5237ca2d34afbbe2697b174ea0107
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-05-21 10:11:04 -0300

    Criação da API para a classe Editora e criação da API para Autor e Livro

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
     "livraria",
 ]
 
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- livraria/admin.py
+++ livraria/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from livraria.models import Categoria, Editora
+from livraria.models import Autor, Categoria, Editora, Livro
 
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
