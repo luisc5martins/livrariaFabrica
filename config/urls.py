e3bf17e62bb8e2965494ebff6b10c543ad882fdb
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-07-01 15:26:40 -0300

    Habilitando o Swagger e Redoc usando DRF Spectacular

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,45 @@
-from django.contrib import admin
-from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from livraria.views import CategoriaViewSet, EditoraViewSet
 from usuario.router import router as usuario_router
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
 
 urlpatterns = [
+    path("api/media/", include(uploader_router.urls)),
     path("admin/", admin.site.urls),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
     path("api/", include(usuario_router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
--- livraria/models/livro.py
+++ livraria/models/livro.py
@@ -1,5 +1,6 @@
 from django.db import models
 from livraria.models import Categoria, Editora, Autor
+from uploader.models import Image
 
 
 class Livro(models.Model):
@@ -16,6 +17,15 @@ class Livro(models.Model):
         Editora, on_delete=models.PROTECT, related_name="livros"
     )
     autores = models.ManyToManyField(Autor, related_name="livros")
+
+    capa = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None,
+    )
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
